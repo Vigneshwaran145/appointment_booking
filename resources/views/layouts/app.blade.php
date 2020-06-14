cb46449b9103ae16887262f521854cc3c3467a01
Vigneshwaran145 <36476876+Vigneshwaran145@users.noreply.github.com>
2020-06-14 19:30:36 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -110,7 +110,7 @@
                             @endif
                         @else
                         @if(Auth::user()->role != 'doctor' )
-                            <a class="navbar-brand" href="/bookAppointment/create">Book Appointment</a>
+                            <a class="navbar-brand" href="{{ route('bookAppointment') }}">Book Appointment</a>
                         @endif
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
